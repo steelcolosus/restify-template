c01e074c536a0e979e8c3270e2ea9be891f01eab
Eduardo Aviles <eduardo.avilesj@gmail.com>
2019-01-26 12:25:28 -0600

    adding decorator for routes

--- src/services/baseService.ts
+++ src/services/baseService.ts
@@ -1,18 +1,38 @@
 import { DatabaseProvider } from "../database";
-import { Repository } from "typeorm";
+import { Repository, ObjectLiteral, ObjectType, DeepPartial } from "typeorm";
 
 export abstract class BaseService<T> {
 
-    clazz: { new(): T; };
 
-    private getRepository() {
-        return DatabaseProvider.getConnection()
-            .then(connection => connection.getRepository(this.clazz));
 
+    constructor(public clazz: new () => T) {
+
+    }
+
+    private async getRepository() {
+        const connection = await DatabaseProvider.getConnection();
+        return await connection.getRepository(this.clazz);
     }
 
     public async getById(id: number): Promise<T> {
-        return await this.getRepository().then(repo => repo.findOne(id));
+        const repository = await this.getRepository();
+        return await repository.findOne(id);
     }
 
+    public async save(customer: DeepPartial<T>): Promise<T> {
+        const repository = await this.getRepository();
+        return await repository.save(customer);
+    }
+    public async list(): Promise<T[]> {
+        const repository = await this.getRepository();
+        return await repository.find();
+    }
+
+    public async delete(id: number): Promise<boolean> {
+        const repository = await this.getRepository();
+        let customer = await this.getById(id);
+        return await repository.remove(customer) ? true : false;
+    }
+
+    public abstract async update(entity: T): Promise<T>;
 }
